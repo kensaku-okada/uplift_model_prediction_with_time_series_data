32537e6adce5d729492bea62f2fe7581e4cdb114
kensaku okada <kensaku.okada@jdsc.ai>
2021-04-20 22:18:43 +0900

    salesandcost.rda imported

--- notebook/import_public_data.py
+++ notebook/import_public_data.py
@@ -21,27 +21,37 @@ import rdata
 def download_a_file_from_url(url: str, destination_path):
     urllib.request.urlretrieve(url, destination_path)
     
-    
 def import_rda_file(import_path, dataframe_name: str):
 
     parsed = rdata.parser.parse_file(import_path)
     converted = rdata.conversion.convert(parsed)
     return converted[dataframe_name]    
 
-public_file_url: str = "https://github.com/google/GeoexperimentsResearch/raw/master/data/geoassignment.rda"
+def import_external_file_as_dataframe(public_file_url, data_name, extension):
 
-absolute_dir = pathlib.Path().resolve()
-data_name = "geoassignment"
-file_name = data_name + ".rda"
+    absolute_dir = pathlib.Path().resolve()
+    file_name = data_name + extension
 
-destination_path = absolute_dir.parent.joinpath("data", "public_data", file_name)
+    destination_path = absolute_dir.parent.joinpath("data", "public_data", file_name)
 
-download_a_file_from_url(public_file_url, destination_path)
+    download_a_file_from_url(public_file_url, destination_path)
 
-public_df = import_rda_file(import_path=destination_path, dataframe_name=data_name)
+    return import_rda_file(import_path=destination_path, dataframe_name=data_name)
+
+
 
 # -
 
-public_df.head
+public_file_url: str = "https://github.com/google/GeoexperimentsResearch/raw/master/data/geoassignment.rda"
+geoassignment_df = import_external_file_as_dataframe(public_file_url, data_name="geoassignment", extension=".rda")
+
+
+geoassignment_df.head
+
+public_file_url: str = "https://github.com/google/GeoexperimentsResearch/raw/master/data/salesandcost.rda"
+salesandcost_df = import_external_file_as_dataframe(public_file_url, data_name="salesandcost", extension=".rda")
+
+
+salesandcost_df.head
 
 
